f0b97006124692209585dbe6805c4216ebbbec08
ParkJihoo <jihoosyudy1@gmail.com>
2025-10-17 17:26:51 +0900

    -- 248p

--- firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -77,5 +78,13 @@ public class MemberController {
         Member target = memberRepository.findById(memberEntity.getId()).orElse(null);
         if(target != null) memberRepository.save(memberEntity);
         return "redirect:/members/" + memberEntity.getId();
+    }
+
+    /**
+     * DELETE
+     */
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+
     }
 }
