07c39b77614de8db69bd1846f64c86509c9f489a
ParkJihoo <jihoosyudy1@gmail.com>
2025-10-20 15:03:50 +0900

    16장 웹 페이지에서 댓글 목록 보기

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.dto.CommentDto;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,6 +21,8 @@ import java.util.List;
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
+    @Autowired
+    private CommentService commentService;
 
     /**
      * CREATE
@@ -51,7 +55,9 @@ public class ArticleController {
         log.info("id: " + id);
         // 1. id를 조회해서 데이터 가져오기
         Article articleEntity = articleRepository.findById(id).orElse(null);
+        List<CommentDto> commentDtos = commentService.comments(id);
         // 2. 모델에 데이터 등록하기
+        model.addAttribute("commentDtos", commentDtos);
         model.addAttribute("article", articleEntity);
         // 3. 뷰 페이지 반환하기
         return "articles/show";
