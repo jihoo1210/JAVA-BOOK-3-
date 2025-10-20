e9bd4cf8a826fe68be3487b31af09cb24dc7f53b
ParkJihoo <jihoosyudy1@gmail.com>
2025-10-20 11:30:23 +0900

    14장 댓글 엔티티와 레파지토리 만들기

---
+++ firstproject/src/main/java/com/example/firstproject/entity/Comment.java
@@ -0,0 +1,28 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+public class Comment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String body;
+}
